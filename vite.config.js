995ea2e6338d99af1589702d71fcdd17ec3d1a87
DanielLenoz <danielstivenxx@outlook.com>
2023-08-15 19:44:45 -0500

    changes ubase url in vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DanielLenoz-blogV3/'
+  base: 'https://daniellenoz.github.io/DanielLenoz-blogV3'
 })
