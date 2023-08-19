e8e26344f919319bd9ebb8cd721acb8069bc8ee4
DanielLenoz <danielstivenxx@outlook.com>
2023-08-19 14:42:27 -0500

    created desing Blog in iphone

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -16,30 +16,30 @@ function Blog() {
     setfilterData(filterDataArea)
   }, [id])
 
+  
+
   return (
-    <>
+    <main className='px-5 bg-slate-100'>
       {filterData.map((blog) => {
         return (
           <>
-            <section key={blog.title}>
-              <figure className="relative z-10">
+            <section className="relative mb-5" key={blog.title}>
+              <figure className="z-10 mx-auto relative mt-4 md:h-[280px] md:w-[590px] lg:h-[293px] lg:w-[890px] xl:h-[367px] xl:w-[1252px]">
                 <picture>
                   <source media="(min-width:945px)" srcSet={blog.imgDesktop} />
                   <img
-                    className="rounded-2xl bg-cover bg-center bg-no-repeat"
+                    className="rounded-2xl bg-cover bg-center bg-no-repeat md:h-[280px] md:w-[590px] lg:h-[293px] lg:w-[890px] xl:h-[367px] xl:w-[1252px]"
                     src={blog.imgIphone}
                     alt={blog.altDescription}
                   />
                 </picture>
               </figure>
-              <div className="diamond"></div>
-              <div className="diamond"></div>
+              <div className="diamond absolute -bottom-40 -left-20 h-44 w-40 "></div>
+              <div className="diamond absolute -bottom-20 -right-20 h-44 w-40"></div>
             </section>
-            <section className="z-10 grid">
-              <h1 className=" text-center font-oswald text-xl font-bold">
-                {blog.title}
-              </h1>
-              <article className="mt-2 flex justify-between">
+            <section className="z-10 relative">
+              <h1 className=" font-oswald text-xl font-bold">{blog.title}</h1>
+              <article className="mb-3 mt-1 flex justify-between">
                 <p
                   className={`${
                     blog.hashtag === '#Blogs'
@@ -63,12 +63,13 @@ function Blog() {
         )
       })}
 
-      <section className="relative mb-16">
-        <div className="diamond absolute -left-20 -top-20 h-44 w-40"></div>
-        <div className="diamond absolute -right-20 -top-20 h-44 w-40"></div>
+      <section className="relative mb-10 mt-4">
+        <p className='text-center font-oswald text-xl font-bold mb-6'>Ultimos Blogs</p>
+        <div className="diamond absolute -left-20 -top-7 h-44 w-40"></div>
+        <div className="diamond absolute -right-20 -top-7 h-44 w-40"></div>
         <Cards blogData2={blogData} filterData={filterData} />
       </section>
-    </>
+    </main>
   )
 }
 
